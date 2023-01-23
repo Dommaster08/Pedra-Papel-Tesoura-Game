e57d9ea2bb5c3450097bdd397fe5621f7369c012
Dommaster08 <luancx7@gmail.com>
2023-01-23 10:59:37 -0300

    adicionado estilos e atualização de placar

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
  
-const computerChoice = ['Pedra', 'Papel', 'Tesoura']
 const placar = document.querySelector('#placar-box')
 const resultado = document.querySelector('#result')
 const pedra = document.querySelector('.pedra')
@@ -7,63 +6,69 @@ const papel = document.querySelector('.papel')
 const tesoura = document.querySelector('.tesoura')
 const gameBox = document.getElementById('game-box')
 const restartGame = document.getElementById('restart-box')
-
 let userScore = 0
 let pcScore = 0
 
-
-const randomChoice = computerChoice[Math.floor(Math.random() * computerChoice.length)]   
-
-
-restartGame.addEventListener('click', ()=>{
-    location.reload()
-})
-
-
-const atualizaPlacar = () =>{
+  
+const atualizaPlacar = () => {
     placar.innerHTML = ` Você: ${userScore} <br>
     Computador:${pcScore}`     
 }
 
-
 const checkWinner = (choice) => {
-    const userGanha = choice === 'Pedra' && randomChoice === 'Tesoura' || 
-    choice === 'Papel' && randomChoice === 'Pedra' ||
-    choice === 'Tesoura' && randomChoice === 'Papel'
+    const pcChoice = compChoice()
+
+    const userGanha = choice === 'Pedra' && pcChoice === 'Tesoura' || 
+    choice === 'Papel' && pcChoice === 'Pedra' ||
+    choice === 'Tesoura' && pcChoice === 'Papel'
    
-    const pcGanha = randomChoice === 'Pedra' && choice === 'Tesoura' || 
-    randomChoice === 'Papel' && choice === 'Pedra' ||
-    randomChoice === 'Tesoura' && choice === 'Papel'
+    const pcGanha = pcChoice === 'Pedra' && choice === 'Tesoura' || 
+    pcChoice === 'Papel' && choice === 'Pedra' ||
+    pcChoice === 'Tesoura' && choice === 'Papel'
     
     if(userGanha){
+        userScore++
         gameBox.style.display = 'none'
         resultado.innerHTML = `<h2>Você Ganhou!</h2><br>
-        Computador: ${randomChoice} <br> Jogador: ${choice}`
-        userScore++
-        atualizaPlacar()
+        Computador: ${pcChoice} <br> Jogador: ${choice}`
         restartGame.style.display = 'flex'
+        atualizaPlacar()
 
     }else if(pcGanha){
+        pcScore++
         gameBox.style.display = 'none'
         resultado.innerHTML = `<h2>Computador Ganhou!</h2><br>
-        Computador: ${randomChoice} <br> Jogador: ${choice}`
-        pcScore++
-        atualizaPlacar()
+        Computador: ${pcChoice} <br> Jogador: ${choice}`
         restartGame.style.display = 'flex'
+        atualizaPlacar()
     }
 }
+
+const compChoice = () => {
+    const computerChoice = ['Pedra', 'Papel', 'Tesoura']
+    const randomChoice = computerChoice[Math.floor(Math.random() * computerChoice.length)] 
+    return randomChoice
+}
+
+restartGame.addEventListener('click', ()=>{
+    gameBox.style.display = 'flex'
+    restartGame.style.display = 'none'
+    resultado.innerHTML = ``
+})
 
 const play = (a) => {
-    if(randomChoice === a){
+    const pcChoice = compChoice()
+    
+    if(pcChoice === a){
         resultado.innerHTML = `<h2>Deu Empate!</h2><br>
-        Computador: ${randomChoice} <br> Jogador: ${a}`
+        Computador: ${pcChoice} <br> Jogador: ${a}`
         gameBox.style.display = 'none'
         restartGame.style.display = 'flex'
-    }else{
+    }else{ 
         checkWinner(a)
     }
+    
 }
-
 
 pedra.addEventListener('click', ()=>{
     play('Pedra')
